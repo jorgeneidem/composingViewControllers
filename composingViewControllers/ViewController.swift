a9d47872882b477f25db7a02a80be82899d70c1e
Jørgen Eidem <Jorgen.Eidem@nrk.no>
2018-02-01 15:50:53 +0100

    Initial Commit

---
+++ composingViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  composingViewControllers
+//
+//  Created by Jørgen Eidem on 01/02/2018.
+//  Copyright © 2018 Jørgen Eidem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
