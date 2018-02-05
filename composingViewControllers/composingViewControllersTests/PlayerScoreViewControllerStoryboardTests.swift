71b93d416970d9d75f0044daf4d909291d4669c2
jorgeneidem <jorgen.eidem@gmail.com>
2018-02-05 08:02:36 +0100

    Setting up unit tests

---
+++ composingViewControllers/composingViewControllersTests/PlayerScoreViewControllerStoryboardTests.swift
@@ -0,0 +1,10 @@
+//
+//  Copyright © 2018 Jørgen Eidem. All rights reserved.
+//
+
+import XCTest
+@testable import composingViewControllers
+
+class PlayerScoreViewControllerStoryboardTests: XCTestCase {
+    
+}
