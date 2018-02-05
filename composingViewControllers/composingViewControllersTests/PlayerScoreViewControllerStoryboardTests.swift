db196f06817f94b9fd36d16a1276eb268c164f17
Jørgen Eidem <Jorgen.Eidem@nrk.no>
2018-02-05 15:09:53 +0100

    Tests for playerOne

--- composingViewControllers/composingViewControllersTests/PlayerScoreViewControllerStoryboardTests.swift
+++ composingViewControllers/composingViewControllersTests/PlayerScoreViewControllerStoryboardTests.swift
@@ -7,4 +7,35 @@ import XCTest
 
 class PlayerScoreViewControllerStoryboardTests: XCTestCase {
     
+    func test_playerOneStoryboardInitionalViewController_isPlayerScoreViewController() {
+        let storyboard = UIStoryboard(name: "PlayerOne", bundle: nil)
+        XCTAssertTrue(storyboard.instantiateInitialViewController() is PlayerScoreViewController)
+    }
+    
+    func test_playerOneStoryboard_nameSetter_updatesNameLabel() {
+        let vc = makePlayerScoreViewController(storyboard: makePlayerOneStoryboard())
+        vc.name = "Some name"
+        XCTAssertEqual(vc.nameLabel?.text, "Some name")
+    }
+    
+    func test_playerOneStoryboard_scoreSetter_updatesScoreLabel() {
+        let vc = makePlayerScoreViewController(storyboard: makePlayerOneStoryboard())
+        vc.score = "Some score"
+        XCTAssertEqual(vc.scoreLabel?.text, "Some score")
+    }
+    
+    // Helpers:
+    
+    private func makePlayerOneStoryboard() -> UIStoryboard {
+        return UIStoryboard(name: "PlayerOne", bundle: nil)
+    }
+    
+    private func makePlayerScoreViewController(storyboard: UIStoryboard) -> PlayerScoreViewController {
+        let vc = storyboard.instantiateInitialViewController() as! PlayerScoreViewController
+        _ = vc.view
+        return vc
+    }
+    
+    
 }
+
